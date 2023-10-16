65ffc6dfc09c7dc34e18d8323db71e32ed540532
Lyriom <josueriera23@hotmail.com>
2023-10-16 13:06:15 -0500

    esto es una preuba

---
+++ untitled/src/HolaMundo.java
@@ -0,0 +1,29 @@
+public class HolaMundo {
+
+        public static void main(String[] args) {
+
+            String saludar = "Josue Riera";
+
+            System.out.println(saludar);
+            System.out.println("saludar.toUpperCase()= " +saludar.toUpperCase());
+            int numero=10;
+            System.out.println("numero="+numero);
+            boolean valor = true;
+            if (valor){
+                System.out.println("numero="+numero);
+                int numero2 = 5;
+            }
+            var numero3= "15";
+            String nombre;
+
+            nombre= "Josue";
+            if (numero >10){
+                nombre= "juan";
+
+            }
+            System.out.println(nombre);
+        }
+
+int persona=5;
+
+    }
---
+++ untitled/src/Primitivos.java
@@ -0,0 +1,28 @@
+public class Primitivos {
+    public static void main(String[] args) {
+        byte numeroByte=17;
+
+        System.out.println("numeroByte = "+ numeroByte);
+        System.out.println("tipo de byte a "+ Byte.BYTES);
+        System.out.println("tipo de byte a "+ Byte.SIZE);
+        System.out.println("valor maximo de un byte es "+ Byte.MAX_VALUE);
+        System.out.println("valor minimo de un byte es "+ Byte.MIN_VALUE);
+
+        short numeroShot= 3600;
+        System.out.println("tipo de short a "+ Short.BYTES);
+        System.out.println("tipo de short a "+ Short.SIZE);
+        System.out.println("valor maximo de un short es a "+ Short.MAX_VALUE);
+        System.out.println("valor minimo de un short es a "+ Short.MIN_VALUE);
+
+        int numeroInt= 32222;
+        System.out.println("tipo de int a "+ Integer.BYTES);
+        System.out.println("tipo de int a "+ Integer.SIZE);
+        System.out.println("valor maximo de un int es a "+ Integer.MAX_VALUE);
+        System.out.println("valor minimo de un int es a "+ Integer.MIN_VALUE);
+
+        long numeroLong = 235532;
+
+
+
+    }
+}
